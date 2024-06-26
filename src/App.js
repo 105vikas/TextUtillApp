4d4b92b5215523d87c22fdf173761609ffd86388
Vikas Yadav <vikasyadav@Vikass-MacBook-Air.local>
2024-06-26 20:59:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import React, { useState } from 'react';
+
+
 
 function App() {
+
+
+  const removebodycls=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+  }
+
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+  const toggleModee = (cls) => {
+    removebodycls();
+    document.body.classList.add('bg-'+cls)
+  }
+  const toggleMode=()=>{
+    if (mode === 'dark') {
+      setMode('light')
+      document.body.style.textcolor = 'red';
+      document.body.style.backgroundColor = 'white';
+      showalert("Light Mode is enable", "success");
+      document.title = 'TextUtil'
+
+    }
+    else {
+      setMode('dark')
+      document.body.style.backgroundColor = '#102436';
+      showalert("Dark Mode is enable", "success");
+      document.title = 'TextUtil-DarkMode'
+    }
+  }
+  const showalert = (massage, type) => {
+    setAlert({
+      msg: massage,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title="vikas" about="I am About" mode={mode} toggleMode={toggleMode} toggleModee={toggleModee} removee={removebodycls}/>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analysis below" alert={showalert} mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
